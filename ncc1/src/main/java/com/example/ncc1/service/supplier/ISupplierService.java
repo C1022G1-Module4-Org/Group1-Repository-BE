32a0f3a06181788c6ffe92adbe2632427431c117
anhhuy12345 <thienhung190492@gmail.com>
2023-03-17 15:37:49 +0700

    cs

---
+++ ncc1/src/main/java/com/example/ncc1/service/supplier/ISupplierService.java
@@ -0,0 +1,23 @@
+package com.example.ncc1.service.supplier;
+
+import com.example.ncc1.model.supplier.Supplier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface ISupplierService {
+    Page<Supplier> findAll(Pageable pageable);
+
+
+    Optional<Supplier> deleteById(int id);
+
+    void save(Supplier supplier);
+
+    Optional<Supplier> findById(int id);
+
+    List<Supplier> listSearchByName(String name);
+}
